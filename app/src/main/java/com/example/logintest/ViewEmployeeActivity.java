d387f0246445316680e94446a4a9acc874dc0d9b
suzu_SSH_Key <avril8319@gmail.com>
2020-11-01 15:45:38 -0800

    Created 'Add Employee' button in View Employee page.

--- app/src/main/java/com/example/logintest/ViewEmployeeActivity.java
+++ app/src/main/java/com/example/logintest/ViewEmployeeActivity.java
@@ -10,13 +10,22 @@ import android.widget.Button;
 public class ViewEmployeeActivity extends AppCompatActivity {
 
     Button btn_back;
+    Button btn_addEmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_employee);
 
+        btn_addEmp = findViewById(R.id.btnAddEmp);
         btn_back = findViewById(R.id.btnBack);
+
+        btn_addEmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewEmployeeActivity.this, AddEmployeeActivity.class));
+            }
+        });
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
