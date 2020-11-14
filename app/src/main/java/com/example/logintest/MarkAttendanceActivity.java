eeb1543a4229761face9686b73e09b13343aed4f
AndroidProjectGroup5 <EmployeeManagement>
2020-11-13 22:17:30 -0800

    MarkAttendanceActivity, strings.xml

--- app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
+++ app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
@@ -2,21 +2,53 @@ package com.example.logintest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MarkAttendanceActivity extends AppCompatActivity {
 
     Button btn_attend, btn_back;
+    private TextView attendance;
+    Calendar c = Calendar.getInstance(); // gets the date
+    DateFormat fmtDate = DateFormat.getDateInstance(); // format the date displayed in the app
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            c.set(Calendar.YEAR, i);
+            c.set(Calendar.MONTH, i1);
+            c.set(Calendar.DAY_OF_MONTH, i2);
+            attendance.setText("Date: " + fmtDate.format(c.getTime()));
+        }
+    }; // event listener after user select a date from calendar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mark_attendance);
+
+        attendance = (TextView) findViewById(R.id.txtAttend);
+        Button button = (Button) findViewById(R.id.btnSetDate);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MarkAttendanceActivity.this,
+                        d,
+                        c.get(Calendar.YEAR),
+                        c.get(Calendar.MONTH),
+                        c.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         btn_back = findViewById(R.id.btnBackHomeMarkAttend);
         btn_attend = findViewById(R.id.btnSaveAttendance);
