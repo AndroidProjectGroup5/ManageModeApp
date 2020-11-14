3461a777ccae13813ad5e780b1a3d4db666daa2e
AndroidProjectGroup5 <EmployeeManagement>
2020-11-13 21:57:29 -0800

    MarkAttendanceActivity, strings.xml

--- app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
+++ app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MarkAttendanceActivity extends AppCompatActivity {
@@ -19,7 +20,6 @@ public class MarkAttendanceActivity extends AppCompatActivity {
 
         btn_back = findViewById(R.id.btnBackHomeMarkAttend);
         btn_attend = findViewById(R.id.btnSaveAttendance);
-
 
         btn_attend.setOnClickListener(new View.OnClickListener() {
             @Override
