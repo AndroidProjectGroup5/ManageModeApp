09e5cda1c1f5f8204ed0dac026f3c41fc3e6173a
AndroidProjectGroup5 <EmployeeManagement>
2020-11-15 15:43:34 -0800

    DatabaseHelper_tryingAttendanceTable

--- app/src/main/java/com/example/logintest/DatabaseHelper.java
+++ app/src/main/java/com/example/logintest/DatabaseHelper.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    static String name = "managemod";
+    static String name = "database21";
     static int version = 1;
 
     private static final String TAG = DatabaseHelper.class.getSimpleName();
@@ -21,6 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     //################# SQL codes #####################//
     //String createTable = "CREATE TABLE if not exists 'user' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'username' VARCHAR NOT NULL, 'password' VARCHAR NOT NULL, 'isAdmin' BOOL)";
+
     String user = "CREATE TABLE if not exists 'user' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "'EmployeeName' VARCHAR NOT NULL, " +
             "'username' VARCHAR NOT NULL," +
@@ -36,12 +37,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             "'TaskAssignee' VARCHAR NOT NULL," +
             " 'TaskStatus' VARCHAR NOT NULL)";
 
+    /*
+    String attendance = "CREATE TABLE 'attendance' ("+
+            "'EmployeeID'	INTEGER NOT NULL," +
+            "'EmployeeName'	VARCHAR NOT NULL," +
+            "'AttDate'	TEXT NOT NULL," +
+            "'ClockIn'	TEXT NOT NULL," +
+            "'ClockOut'	TEXT NOT NULL," +
+            "FOREIGN KEY('EmployeeID') REFERENCES 'users'('id') ON DELETE CASCADE ON UPDATE CASCADE);";
+    */
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(user);
         db.execSQL(task); // right way would be to check if table exists first, this just simplifies
+        //db.execSQL(attendance); // right way would be to check if table exists first, this just simplifies
+
         fillEmployees(db);
         fillTasks(db);
+        //fillAttendances(db);
     }
 
     public void fillEmployees(SQLiteDatabase db) {
@@ -55,7 +69,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(insertUsers);
     }
 
-
     private void fillTasks(SQLiteDatabase db) {
         String insertTasks = "INSERT INTO task (TaskName, TaskDescription, TaskAssignee, TaskStatus) VALUES (" +
                 "'Cleaning', 'Clean the premises after the workday is over.', 'Joe', 'Completed'), " +
@@ -63,6 +76,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 "('Locking down', 'Locking the facilities', 'Mary', 'Not started')";
         db.execSQL(insertTasks);
     }
+
+    /*
+    private void fillAttendances(SQLiteDatabase db) {
+        String insertAttendances = "INSERT INTO attendance (EmployeeName, AttDate, ClockIn, ClockOut) VALUES (" +
+                "'Danilo Andrade', '2020-11-05', '09:00:00.000', '04:00:00.000'), " +
+                "('Andrea Cruz', '2020-10-26', '10:30:00.000', '06:00:00.000')," +
+                "('Jaimish Patel', '2020-09-26', '09:30:00.000', '05:00:00.000')," +
+                "('Suzuka Natsuhara', '2020-11-13', '08:30:00.000', '07:00:00.000')," +
+                "('Danilo Andrade', date('now'), '09:30:00.000', '04:45:00.000')";
+        db.execSQL(insertAttendances);
+    }
+    */
 
     public boolean addUser (Employee emp) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -94,8 +119,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }else {
             return true;
         }
-
     }
+
+    /*
+    public boolean saveAttendance (Attendance att) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("EmployeeName", att.geteName());
+        cv.put("AttDate", att.getAttDate());
+        cv.put("ClockIn", att.getaClockIn());
+        cv.put("ClockOut", att.getaClockOut());
+
+        long insert = db.insert("attendance", null, cv);
+        if(insert == -1){
+            return false;
+        }else {
+            return true;
+        }
+    }
+    */
 
     public void insertUsers(ContentValues contentValues){
         getWritableDatabase().insert("user","", contentValues);
@@ -104,6 +146,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void insertTasks(ContentValues contentValues){
         getWritableDatabase().insert("task","", contentValues);
     }
+
+    /*
+    public void insertAttendances(ContentValues contentValues){
+        getWritableDatabase().insert("attendance","", contentValues);
+    }
+    */
+
 
     // checks to see if the arguments matches any record in the table
     public boolean isLoginValid(String username, String password){
@@ -120,8 +169,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         Log.w(DatabaseHelper.class.getName(),
@@ -129,10 +176,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                         + i1 + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS 'user'");
         db.execSQL("DROP TABLE IF EXISTS 'task'");
+        db.execSQL("DROP TABLE IF EXISTS 'attendance'");
         onCreate(db);
     }
 }
 
+
+// Danilo's spare code to test and adjust
 /*    String status = "CREATE TABLE 'Status' ('StatusID' INTEGER NOT NULL," +
             "'Name' TEXT NOT NULL PRIMARY KEY('StatusID'));";
 
@@ -144,8 +194,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             "'TaskDescription' TEXT NOT NULL," +
             "FOREIGN KEY('userID') REFERENCES 'user'('id'));";
 
-    String attendance = "CREATE TABLE 'attendance' ("+
-            "'EmployeeID'	INTEGER NOT NULL," +
-            "'Date'	NUMERIC NOT NULL," +
-            "'Status'	BLOB NOT NULL," +
-            "FOREIGN KEY('EmployeeID') REFERENCES 'users'('id') ON DELETE CASCADE ON UPDATE CASCADE);";*/
+    */
