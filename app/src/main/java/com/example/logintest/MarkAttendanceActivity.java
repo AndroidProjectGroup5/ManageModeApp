8a7668fbc50f107ee889422dfd33217a634378f8
AndroidProjectGroup5 <EmployeeManagement>
2020-11-15 09:24:36 -0800

    MarkAttendanceActivity

--- app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
+++ app/src/main/java/com/example/logintest/MarkAttendanceActivity.java
@@ -3,23 +3,29 @@ package com.example.logintest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.sql.Time;
 import java.text.DateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 public class MarkAttendanceActivity extends AppCompatActivity {
 
     Button btn_attend, btn_back;
-    private TextView attendance;
+    private TextView attendance, clockInOut;
+
     Calendar c = Calendar.getInstance(); // gets the date
     DateFormat fmtDate = DateFormat.getDateInstance(); // format the date displayed in the app
+    DateFormat fmtTime = DateFormat.getTimeInstance(); // format the date displayed in the app
 
     DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -30,6 +36,15 @@ public class MarkAttendanceActivity extends AppCompatActivity {
             attendance.setText("Date: " + fmtDate.format(c.getTime()));
         }
     }; // event listener after user select a date from calendar
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int h, int h1) {
+            c.set(Calendar.HOUR_OF_DAY, h);
+            c.set(Calendar.MINUTE, h1);
+            clockInOut.setText("ClockIn: " + fmtTime.format(c.getTime()));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +63,12 @@ public class MarkAttendanceActivity extends AppCompatActivity {
                         c.get(Calendar.MONTH),
                         c.get(Calendar.DAY_OF_MONTH)).show();
             }
+            //public void onClick(View v) {
+            //    new TimePickerDialog(MarkAttendanceActivity.this,
+            //            t,
+            //            c.get(Calendar.HOUR_OF_DAY),
+            //            c.get(Calendar.MINUTE)).show();
+            //}
         });
 
         btn_back = findViewById(R.id.btnBackHomeMarkAttend);
